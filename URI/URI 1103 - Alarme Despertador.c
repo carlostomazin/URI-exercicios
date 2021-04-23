b12931a42c33eb705f09d0c3379646cb9bfd5350
Carlos Augusto Faustino Tomazin <65822302+carlostomazin@users.noreply.github.com>
2021-04-22 22:09:04 -0300

    Add files via upload

---
+++ URI/URI 1103 - Alarme Despertador.c
@@ -0,0 +1,27 @@
+//20006998		1103	Alarme Despertador	Accepted	C	0.000	06/10/2020 17:01:50
+#include <stdio.h>
+
+int main() {
+
+  int hora1,min1,hora2,min2;
+  int min;
+
+  while(1){
+
+  printf("Digite hora e minutos (##:##)\n");
+  scanf("%d %d %d %d",&hora1, &min1, &hora2, &min2);
+
+  if (!hora1 && !min1 && !hora2 && !min2)
+    break;
+  
+  if (hora1 > hora2 || hora1 == hora2 && min1 > min2)
+    hora2 += 24;
+
+  min = (hora2*60 + min2) - (hora1*60 + min1);
+
+  printf("%d\n", min);
+
+  
+  }
+  return 0;
+}
