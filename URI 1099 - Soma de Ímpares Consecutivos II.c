d8b2af210c57da3c13059d3cfcab1b8494ceafb6
carlostomazin <carlos.tomazin2@gmail.com>
2021-05-21 18:41:56 -0300

    exc

--- URI 1099 - Soma de Ímpares Consecutivos II.c
+++ URI 1099 - Soma de Ímpares Consecutivos II.c
@@ -2,7 +2,8 @@
 
 int main(){
 
-    int N,ctd,x,y;
+    int N,ctd,i,x,y;
+    int soma=0,aux;
 
     scanf("%d",&N);
 
@@ -10,11 +11,26 @@ int main(){
 
         scanf("%d %d",&x,&y);
 
-        
+        if(x < y){
+            aux = x;
+            x = y;
+            y = aux;
+        }
 
+        if(y+1 == x || y == x){
+            soma = 0;
+        }else{
+            for(i=x-1; i>=y+1; i--){
+                if(i % 2 != 0){
+                    soma+=i;
+                }
+            }
+        }
+
+        printf("%d\n",soma);
+        soma = 0;
 
     }
-
-
+    
     return 0;
 }
